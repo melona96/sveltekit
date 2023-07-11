93d45278fff902cf64de6dc82125b1d6fc2876a5
Naejin Nam <salvia1223@naver.com>
2023-07-11 18:34:43 +0900

    #3 about 페이지에 이미지 삽입 및 글 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="#">Tokyo Gourmet</a>
+        <a class="navbar-brand" href="/">Tokyo Gourmet</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -15,7 +15,7 @@
 <header class="bg-dark py-5">
     <div class="container px-4 px-lg-5 my-5">
         <div class="text-center text-white">
-            <h1 class="display-4 fw-bolder">Tokyo's Gourmet Information</h1>
+            <h1 class="display-4 fw-bolder">Tokyo's Gourmet</h1>
             <p class="lead fw-normal text-white-50 mb-0">도쿄의 가장 핫한 맛집들을 찾아보세요.</p>
         </div>
     </div>
