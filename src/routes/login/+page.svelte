8875696c85ffd96ff025a101d56ebbdc58712230
Naejin Nam <salvia1223@naver.com>
2023-07-19 17:41:13 +0900

    #8 BE로 요청 보내기 및 요청 수신 테스트

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,27 +1,30 @@
 <script>
     let userId = '';
     let pwd = '';
-    async function test() {
+    async function testLogin() {
         const info = {
             userId,
             pwd
         };
         try {
             console.log(info)
-            const response = await fetch('/api/login', {
-                method: 'POST',
-                headers: {
+            const response = await fetch('/test/login', {
+                method: 'GET',
+                /*headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(info),
-                credentials: 'include'
+                credentials: 'include'*/
             });
 
+            console.log(response);
             if (response.ok) {
+                console.log('연동 성공');
                 const token = await response.json();
                 // 토큰을 저장하거나 필요한 후속 작업 수행
             } else {
                 // 로그인 실패 처리
+                console.log('연동 실패')
             }
         } catch (error) {
             // 에러 처리
@@ -33,7 +36,7 @@
     <div class="d-flow justify-content-center">
         <div class="d-flex justify-content-center">
             <div class="login-dev">
-                <form class="login-form" on:submit={test}>
+                <form class="login-form" on:submit={testLogin}>
                     <div class="p-5 input-form-div">
                         <div class="input-group mb-3">
                             <span class="input-group-text bg-dark"><i class="bi bi-person-plus-fill text-white"></i></span>
