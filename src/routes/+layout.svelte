76c7256e326ef346f39d7b59baae7dc2f0158cd5
Naejin Nam <salvia1223@naver.com>
2023-07-26 16:18:54 +0900

    #11 글쓰기 BE 연동 테스트완료, 에디터 라이브러리 테스트

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -64,4 +64,3 @@
     <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Kenny Commpany 2023</p></div>
 </footer>
 <!-- Bootstrap core JS-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
--- src/routes/board/write/+page.svelte
+++ src/routes/board/write/+page.svelte
@@ -1,1 +1,56 @@
-<p>글쓰기.</p>
+<script>
+    import {goto} from "$app/navigation"; // goto 함수로 자체적
+
+    let title;
+    let content;
+
+    async function testWrite() {
+        const data = {
+            title,
+            content
+        };
+        try {
+            console.log(data)
+            const response = await fetch('/api/board/write', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data),
+                credentials: 'include'
+            });
+
+            console.log(response);
+            if (response.ok) {
+                console.log('연동 성공');
+                const token = await response.json();
+                // 토큰을 저장하거나 필요한 후속 작업 수행
+                goto('/home')
+            } else {
+                // 로그인 실패 처리
+                console.log('연동 실패')
+            }
+        } catch (error) {
+            // 에러 처리
+        }
+    }
+
+</script>
+
+
+<div class="container px-4 px-lg-5 mt-3">
+    <form on:submit={testWrite}>
+        <div class="mb-3">
+            <input type="text" name="title" class="form-control" id="exampleFormControlInput1" placeholder="제목을 입력하세요." bind:value={title}>
+        </div>
+        <div class="mb-3">
+            <textarea class="summernote" name="editordata" placeholder="내용을 입력하세요." bind:value={content}></textarea>
+        </div>
+        <div id="summernote">Hello Summernote</div>
+
+        <div>
+            <button class="btn btn-outline-secondary" type="submit">작성</button>
+        </div>
+    </form>
+    <br>
+</div>
